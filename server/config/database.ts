37cb0c12bac48fdab0856d38ab1c5b269b1cd787
Princess <maeprincess146@gmail.com>
2024-05-26 15:12:37 +0800

    Bow down to thy Princess~

---
+++ server/config/database.ts
@@ -0,0 +1,20 @@
+// server/config/database.ts
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
